9102dbb00153447fd9f4e19b5bfb7f515ebdb895
Charlotte Bastin <bastinch@ip29.m>
2016-05-25 13:43:10 -0400

    enlevé du texte inutile

--- csv_manager.py
+++ csv_manager.py
@@ -39,30 +39,7 @@ class CsvManager(object):
 					break
 		return {key:self.sep.join(content) for key, content in new_row.iteritems()}
 
-	"""#fonction qui trie les cibles des anticorps (colonne 5) avec le dictionnaire de cibles
-	def filter_csv(self, row):
-		normalized_row = utils.norm_keys(row)
-		for content in new_row["5)antibody"]:
-			if re.search(self.regex_dict[targets]):
-				information = normalized_row
-				if information:
-					new_row["clean_target"].add(information)
-				break
-				
-	#fonction qui cherche la cible dans la colonne 8 ou 9				
-	def assign_target(self,row):
-
-		for row in new_row["clean_target"]:
-			if key == 'tag':
-				for row in new_row["9)genotype"]:	
-					if re.search(self.regex_dict[tagged],):
-						tag = 'ghgjhkkg'
-						if tag:"""
-
-
-
-							
-				
+	
 
 	def write_csv(self, outfile):
 		writer = csv.DictWriter(outfile,
