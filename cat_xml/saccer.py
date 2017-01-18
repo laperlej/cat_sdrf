a8622a91c47f8447f79f3d19c23725d0951fca86
Charlotte Bastin <bastinch@ip29.m>
2017-01-17 22:00:13 -0500

    local changes commited

--- cat_xml/saccer.py
+++ cat_xml/saccer.py
@@ -6,6 +6,7 @@ CELL_TYPE = OrderedDict ([
 	('N/A', '.*') ])
 
 """
+Informations provenant de pombase, fichier pombe_features_cat3chr.txt datant de juin 2016.
 modifications:
 	($a): regex visant à ne rien reconnaitre 
 	BDP1, dans les alias, enlevé 'B"' car problématique avec regex
