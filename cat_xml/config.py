742ae001af2f8186860e22a3f366ee0be9fd4447
charlottou <charlotte.bastin@usherbrooke.ca>
2017-05-08 11:16:04 -0400

    added new fieldnames dict in comments
    in preparation for the removal of the old one

--- cat_xml/config.py
+++ cat_xml/config.py
@@ -124,6 +124,39 @@ FIELDNAMES=OrderedDict([
 	('26)Pubmed ID', lambda title, row: re.search('pubmed_id_idf', title)),
 	('Other', lambda title, row: re.search('.*', title))])
 
+"""
+	('1)identifier', ''),
+	('1,1)Sample_title', ''),
+	('2)filename', ''),
+	('3)organism', ''),
+	('4)clean_assay',''),
+	('5)clean_target', ''),
+	('6)reliability', ''),
+	('7)assaytype', ''),
+	('8)antibody', ''),
+	('9)target', ''),
+	('10)treatment', ''),
+	('11)Material_type',''),
+	('12)clean_celltype', ''),
+	('13)cell_type',''),
+	('14)strain', ''),
+	('15)genotype', ''),
+	('16)platform', ''),
+	('Manufacturer', ''),
+	('17)Sample_description', ''),
+	('18)raw_files', ''),
+	('19)all_supp_files', ''),
+	('20)SRA_accessions', ''),
+	('21)Experiment description', ''),
+	('22)Protocol', ''),
+	('23)Author(s)', ''),
+	('Releasing group', ''),
+	('24)Submission Date', ''),
+	('25)Release Date', ''),
+	('26)Pubmed ID', ''),
+	('Other', '')])
+"""
+
 #assay type dictionnary (WGS:Whole Genome Shotgun sequencing)
 ASSAY_DICO = OrderedDict([
 	('Non-genomic', '(non.genomic|transcriptomic|total\srna|nascent\srna|polya\srna)'),
