aea16ccc938b102ed9604273a9fa992334a8be9e
charlottou <charlotte.bastin@usherbrooke.ca>
2017-05-31 17:50:24 -0400

    still bug with None

--- chip-chip/xml_manager.py
+++ chip-chip/xml_manager.py
@@ -281,9 +281,11 @@ class XmlManager(object):
 								for section in row:
 									row[section] = row[section].replace(key,special_characters[key])
 							self.rows.append(row)
-							if self.duplicate_channels(row) is not 'None':
+							if self.duplicate_channels(row) is not None:
 								print (self.duplicate_channels(row))
 								self.rows.append(self.duplicate_channels(row))
+							else:
+								pass
 
 	def general_sample(self, my_list, section):
 		my_list.append(section)
